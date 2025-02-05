a8cf244d9a7fd871e22cfbc014e787725df33bdd
lucasadilla <lucas.pentlandhyde@gmail.com>
2025-02-04 23:52:05 -0500

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { useEffect } from 'react';
 import { Analytics } from "@vercel/analytics/react";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
     useEffect(() => {
@@ -21,8 +22,17 @@ function MyApp({ Component, pageProps }) {
 
     return (
         <>
+            {/* Global Favicon Setup */}
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <title>Lucas Pentland-Hyde</title> {/* Optional global title */}
+            </Head>
+
             <Component {...pageProps} />
-            <Analytics /> {/* Add Analytics here */}
+            <Analytics />
         </>
     );
 }
